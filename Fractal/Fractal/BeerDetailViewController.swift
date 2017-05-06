4bdbd6c1b4d0f57b85be54b7c2ceb241495f2e8f
Caio Colaiacovo <caiohshot@gmail.com>
2017-05-05 22:18:55 -0400

    Adding views

---
+++ Fractal/Fractal/BeerDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BeerDetailViewController.swift
+//  Fractal
+//
+//  Created by Caio Colaiacovo on 05/05/17.
+//  Copyright © 2017 Caio Colaiacovo. All rights reserved.
+//
+
+import UIKit
+
+class BeerDetailViewController: UIViewController {
+
+    @IBOutlet weak var favoriteIcon: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var tagline: UILabel!
+    @IBOutlet weak var beerDescription: UILabel!
+    
+
+}
---
+++ Fractal/Fractal/BeerListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BeersListViewController.swift
+//  Fractal
+//
+//  Created by Caio Colaiacovo on 05/05/17.
+//  Copyright © 2017 Caio Colaiacovo. All rights reserved.
+//
+
+import UIKit
+
+class BeersListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+
+}
