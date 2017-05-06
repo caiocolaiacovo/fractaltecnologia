ad62e0fc2a3af9f5e1d107b7d8ccec0e29767448
Caio Colaiacovo <caiohshot@gmail.com>
2017-05-05 20:46:45 -0400

    Initializing project.

---
+++ Fractal/Fractal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fractal
+//
+//  Created by Caio Colaiacovo on 05/05/17.
+//  Copyright © 2017 Caio Colaiacovo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
